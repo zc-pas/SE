2057b1ab5e336fd2b44b524f4102d83bc8883b8b
Tianshuo <2746520Z@student.gla.ac.uk>
2023-03-11 16:28:53 +0000

    test

---
+++ src/ae2/src/Period.java
@@ -0,0 +1,35 @@
+package ae2.src;
+
+
+
+public class Period {
+	private int year; // 2023
+	private int startDate; // 0901
+	private int duration; // 10 weeks
+	
+	public Period() {
+		this.year = 2023;
+		this.startDate = 901;
+		this.duration = 20;
+	}
+	
+//	@SuppressWarnings("null")
+//	public Period(int year) {
+//		this.year = year;
+//		this.startDate = (Integer) null;
+//		this.duration = (Integer) null;
+//	}
+	
+	public Period(int year) {
+		this.year = year;
+		this.startDate = 901;
+		this.duration = 20;
+	}
+	
+	public Period(int year, int startDate, int duration) {
+		this.year = year;
+		this.startDate = startDate;
+		this.duration = duration;
+	}
+	
+}
---
+++ src/ae2/src/Teacher.java
@@ -0,0 +1,77 @@
+package ae2.src;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teacher implements Serializable{
+    private String name;    
+    private List<String> courseCapable;
+    static private int nextID = 0;
+    private int teacherId;
+
+    public Teacher(String name){
+        this.teacherId=nextID;
+        Teacher.nextID++;
+        this.name=name;
+        this.courseCapable= new ArrayList<String>();
+    }
+    
+    public Teacher(int ID, String name, ArrayList<String> courseCapable){
+        this.teacherId=ID;
+        this.name=name;
+        this.courseCapable=courseCapable;
+    }
+    
+    public Teacher(String name, String capable){
+        this.teacherId=nextID;
+        Teacher.nextID++;
+        this.name=name;
+        this.courseCapable= new ArrayList<String>();
+        this.courseCapable.add(capable);
+    }
+    
+    public Teacher(String name, ArrayList<String> courseCapable){
+        this.teacherId=nextID;
+        Teacher.nextID++;
+        this.name=name;
+        this.courseCapable=courseCapable;
+    }
+    
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+
+
+    
+    public List<String> getCourseCapable() {
+        return courseCapable;
+    }
+    public void setCourseCapable(ArrayList<String> courseCapable) {
+        this.courseCapable = courseCapable;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString(){
+    	String result = String.format("TeacherID: %-3d Name: %-15s Type: ", teacherId, name);
+        for(String s:courseCapable){
+            result+=String.format("%-10s", s);
+        }
+        return result;
+    }
+    
+
+    
+}
